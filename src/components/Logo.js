5bb489d83febf9a72c7d95e847fa15dfd295bd38
mosmn <ithinkisee8@gmail.com>
2023-07-18 10:05:56 +0800

    updated logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Logo = () => {
   return (
     <div className="logo-container">
-    <span className="logo-text">MyShop</span>
-    <svg className="logo-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-      <path d="M12 2L2 7l10 5 10-5-10-5zm0 11.5L2 18V9l10 5 10-5v9l-10 5z"/>
-    </svg>
-  </div>
+      <span className="logo-text">Threads & Co.</span>
+      <svg className="logo-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path d="M12 2L2 7l10 5 10-5-10-5zm0 11.5L2 18V9l10 5 10-5v9l-10 5z"/>
+      </svg>
+    </div>
   );
 };
 
