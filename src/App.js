7fd172614e4661bd12a84137852b8111d974d793
mosmn <ithinkisee8@gmail.com>
2023-07-22 05:58:22 +0800

    styled home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import './App.css';
+import "./styles/App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="header">
-        <h1>Welcome to Threads & Co.</h1>
-      </header>
-      <main className="main-content">
-        <section className="hero-section">
-          <h2>Discover the Latest Fashion Trends</h2>
-          <p>Explore our collection of stylish clothing and accessories for men and women.</p>
-          <button className="shop-now-btn">Shop Now</button>
-        </section>
-      </main>
-      <footer className="footer">
-        <p>&copy; 2023 Threads & Co. All rights reserved.</p>
-      </footer>
-    </div>
+    <header className="header">
+      <h1>Welcome to Threads & Co.</h1>
+    </header>
+    <main className="main-content">
+      <section className="hero-section">
+        <h2>Discover the Latest Fashion Trends</h2>
+        <p>Explore our collection of stylish clothing and accessories for men and women.</p>
+        <button className="shop-now-btn">Shop Now</button>
+      </section>
+    </main>
+    <footer className="footer">
+      <p>&copy; 2023 Threads & Co. All rights reserved.</p>
+    </footer>
+  </div>
   );
 };
 
